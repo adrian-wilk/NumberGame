b89a4b7b7c33799a53c0f9a29633eacb3b12b4bc
Adrian Wilk <awilk2@gmail.com>
2020-04-08 13:12:05 +0200

    NumberGame is finished and working

--- src/NumberGame.java
+++ src/NumberGame.java
@@ -8,13 +8,31 @@ public class NumberGame {
 
 // randomNumber creates a random integer from 1 to 100.
         int randomNumber = (int) (Math.random() * 100) +1;
+        boolean hasWon = false;
+
         System.out.println("I have randomly chosen a number between 1 and 100");
         System.out.println("Try to guess it.");
-        System.out.println(randomNumber);
 
+Scanner scanner = new Scanner(System.in);
+for(int i=10; i>0; i--){
+    int guess = scanner.nextInt();
+    if (randomNumber < guess) {
+        System.out.println("It's smaller than " + guess + ".");
+    }
+    if(randomNumber > guess){
+        System.out.println("It's bigger than " + guess + ".");
+    }
+    if(randomNumber == guess){
+        hasWon = true;
+        break;
+    }
+}
+if(hasWon){
+System.out.println("CORRECT... YOU WIN!!!");
+}else{
+    System.out.println("Game over. You lose!!!");
+    System.out.println("The number was : "+ randomNumber + ".");
+}
 
     }
-
-
-
 }
